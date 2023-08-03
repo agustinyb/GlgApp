8cc7acd29c6bed6042cd3b7cfa25404f3dacad1e
Agustin <agusdolimpio@gmail.com>
2023-08-02 20:38:41 -0400

    Conectando con el frontend

--- src/main/java/com/glgweb/app/Auth/AuthController.java
+++ src/main/java/com/glgweb/app/Auth/AuthController.java
@@ -3,10 +3,7 @@ package com.glgweb.app.Auth;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,11 +13,13 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @CrossOrigin("http://127.0.0.1:5500")
     @PostMapping(value = "login")
     public  ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @CrossOrigin("http://127.0.0.1:5500")
     @PostMapping(value = "register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
